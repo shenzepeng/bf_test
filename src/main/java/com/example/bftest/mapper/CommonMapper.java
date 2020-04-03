fa8269ef1d4b34e911ce6e9dc2b9b9ea3058437a
mac <806871609@qq.com>
2020-04-03 15:49:37 +0800

    clean doc

--- src/main/java/com/example/bftest/mapper/CommonMapper.java
+++ src/main/java/com/example/bftest/mapper/CommonMapper.java
@@ -4,7 +4,7 @@ import tk.mybatis.mapper.common.Mapper;
 import tk.mybatis.mapper.common.MySqlMapper;
 
 /**
- * @author chenrui
+ * @author asd
  * @date 2018/12/13 20:36
  */
 public interface CommonMapper<T> extends Mapper<T>, MySqlMapper<T> {
